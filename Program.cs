4be2c169350a0d3886f734df97ba936ce8f67dd8
Nadezhda26m <nadezhda-26m@mail.ru>
2022-11-14 11:16:58 +0300

    add method: FillArrayStringsRandom

--- Program.cs
+++ Program.cs
@@ -3,3 +3,20 @@ string[] CreateArray(int size)
 {
     return new string[size];
 }
+
+// 2. Метод заполнения массива строк случайными данными 
+void FillArrayStringsRandom(string[] text, string selectChar,
+                            int lengthItemMin, int lengthItemMax)
+{
+    int size = text.Length;
+    int countChars = selectChar.Length;
+    int lengthItem;
+    for (int i = 0; i < size; i++)
+    {
+        lengthItem = new Random().Next(lengthItemMin, lengthItemMax + 1);
+        for (int j = 0; j < lengthItem; j++)
+        {
+            text[i] += selectChar[new Random().Next(0, countChars)];
+        }
+    }
+}
